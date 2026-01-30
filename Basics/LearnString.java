a7851fef080137bd2b9d5af2f35f95b7b5c07fc6
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:23:22 +0530

    Add LearnString class to demonstrate string operations
    Demonstrates string initialization, length, and character access in Java.

---
+++ Basics/LearnString.java
@@ -0,0 +1,18 @@
+public class LearnString{
+	public static void main(String [] args){
+		
+		String es = "Isuru Isuru Isuru"; // Empty String
+		String name = new String();
+		System.out.println("Name: "+es);
+		System.out.println("Name is: "+name); //Null String
+		
+		//Length of the String
+		System.out.println("Length of the string is: "+es.length());
+		
+		char ps = es.charAt(10);
+		System.out.println("Character in the 10th position is: "+ps);
+		
+		
+		
+	}
+}
