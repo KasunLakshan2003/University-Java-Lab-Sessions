b9667068b72644ba19f2fb2749f6b1c847e70f5b
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 00:55:08 +0530

    Add FirstPro.java to convert inches to meters

---
+++ Basics/FirstPro.java
@@ -0,0 +1,15 @@
+public class FirstPro{
+	
+	public static void main (String [] args){
+		
+		
+		
+		double Cvr = 0.0254 ;
+		double Inches = 10;
+		double Result = Inches*Cvr;
+		
+		System.out.println("Length in meters " + Result);
+		
+		
+	}
+}
