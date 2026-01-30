886b603e46caffab4913d06bf1af05b94b0a4e6f
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:08:42 +0530

    Add PrintArgs2 class to display name and age

---
+++ Basics/PrintArgs2.java
@@ -0,0 +1,9 @@
+public class PrintArgs2{
+	public static void main(String [] args){
+		
+		String name = "Kasun";
+		int age = 22;
+		
+		System.out.printf("My name is %s and I am %d years old",name,age);
+	}
+}
