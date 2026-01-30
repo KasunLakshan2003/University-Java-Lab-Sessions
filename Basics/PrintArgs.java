262633b0d923a1e1a09c56526ed21c4180a97b5c
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:07:45 +0530

    Add PrintArgs class to demonstrate printf usage

---
+++ Basics/PrintArgs.java
@@ -0,0 +1,118 @@
+public class PrintArgs {
+	public static void main(String [] args){
+		/*
+		System.out.printf("Hello %s%n%s%s%n","World!","Hello"," World Java");
+		
+		System.out.printf("Hello %S is %d value\n","type",3);
+		
+		System.out.printf("Hello %s %n","java");
+		System.out.printf("Hello %S %n","java");
+		System.out.printf("Year is %d \n",2026);
+		System.out.printf("Value of PI is %f \n",3.14);
+		System.out.printf("Character %c is a vowel %n",'A');
+		System.out.printf("Hello %s %n","Java");
+		
+		//line seperator
+		System.out.printf("Hello madam\nI am Adam\nWorking as a Devops\n");
+		System.out.printf("Hello madam\t I am Adam \t Working as a Devops\t \n");
+		*/
+		//Rules
+		String name = "Kasun";
+		System.out.printf("%5s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%11s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%5s %n", name);
+		System.out.printf("%5s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%11s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%5s %n", name);
+		System.out.printf("%5s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%35s %n", name);
+		System.out.printf("%33s %n", name);
+		System.out.printf("%31s %n", name);
+		System.out.printf("%29s %n", name);
+		System.out.printf("%27s %n", name);
+		System.out.printf("%25s %n", name);
+		System.out.printf("%23s %n", name);
+		System.out.printf("%21s %n", name);
+		System.out.printf("%19s %n", name);
+		System.out.printf("%17s %n", name);
+		System.out.printf("%15s %n", name);
+		System.out.printf("%13s %n", name);
+		System.out.printf("%11s %n", name);
+		System.out.printf("%9s %n", name);
+		System.out.printf("%7s %n", name);
+		System.out.printf("%5s %n", name);
+		
+		
+		
+	}
+}
