d4e593113d397e8a0c7a2a67d1d5ca78f57d2375
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 00:56:28 +0530

    Add area calculations for circle, square, rectangle, and triangle

---
+++ Basics/area.java
@@ -0,0 +1,38 @@
+public class area{
+	
+	public static void main (String [] args){
+		
+		//area of circle
+		
+		int r = 4;
+		double AreaOfCircle = 3.14 * r * r;
+		System.out.println("Area of the circle : "+AreaOfCircle);
+		
+		//area of square
+		
+		int l = 5;
+		int AreaOfSquare = l * l;
+		System.out.println("Area of the square :"+AreaOfSquare);
+		
+		//area of rectangle
+		
+		int w = 5;
+		int h = 6;
+		int AreaOfRectangle=w * h;
+		System.out.println("Area of the rectangle :"+AreaOfRectangle);
+		System.out.println("Area of the rectangle :"+AreaOfRectangle);
+		
+		//area of triangle
+		
+		int b = 10;
+		int ht = 6;
+		double AreaOfTriangle = 0.5 * b * ht;
+		System.out.println("Area of the triangle :"+AreaOfTriangle);
+		
+		
+		
+		
+
+		
+	}
+}
