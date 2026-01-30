2cd1743e1340025e60d44871c0a78d4fc3943f96
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:13:13 +0530

    Add WaterPark class with height check

---
+++ Basics/WaterPark.java
@@ -0,0 +1,14 @@
+public class WaterPark{
+	public static void main(String [] args){
+		
+		int height = 8;
+		
+		if(5 < height && height < 10){
+			System.out.println("1");
+			
+		}else{
+			System.out.println("0");
+		}	
+		
+	}
+}
