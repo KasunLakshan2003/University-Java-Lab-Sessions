9977f4c7d7a1d8e9a7b761ce042d9c3df356d64c
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:12:50 +0530

    Add Compare class to compare two heights

---
+++ Basics/Compare.java
@@ -0,0 +1,18 @@
+public class Compare{
+	public static void main(String [] args){
+		
+		int height1 = 175;
+		int height2 = 185;
+		
+		if(height1 == height2){
+			System.out.println("True");
+			
+		}else{
+			System.out.println("False");
+		}
+		
+		
+		
+		
+	}
+}
