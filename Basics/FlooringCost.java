2bbd91f834695b54be2a25f41c6e86651a12c2b4
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:14:22 +0530

    Implement FlooringCost calculator
    Calculate flooring cost based on room dimensions and cost per square unit.

---
+++ Basics/FlooringCost.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FlooringCost{
+	public static void main(String [] args){
+		
+		Scanner myscanner = new Scanner(System.in);
+		
+		System.out.print("Enter length of the room: ");
+		double length = myscanner.nextDouble();
+		
+		System.out.print("Enter width of the room: ");
+		double width = myscanner.nextDouble();
+		
+		System.out.print("Enter cost per square root: ");
+		double costPer = myscanner.nextDouble();
+		
+		double TotalArea = length * width;
+		double TotalCost = area * costPer;
+		
+		System.out.print(TotalCost);
+	
+		
+		
+		
+	}
+}
