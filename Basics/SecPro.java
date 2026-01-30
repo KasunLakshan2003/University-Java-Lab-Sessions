65a8b28fdf57a9e1548f5a848eb858265ee27105
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 00:55:45 +0530

    Add SecPro class to convert minutes to years

---
+++ Basics/SecPro.java
@@ -0,0 +1,11 @@
+public class SecPro{
+	
+	public static void main (String [] args){
+		
+		long min = 52560000 ;
+		int Cvr = 60*24*365;
+		double years = min/Cvr; 
+		
+		System.out.println("min " +min+" In years "+ years);
+	}
+}
