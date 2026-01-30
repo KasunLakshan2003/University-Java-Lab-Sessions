2e4f10ec29402ba8130ec2030b044414c02462bf
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 01:01:14 +0530

    Add Unary class demonstrating unary operators

---
+++ Basics/Unary.java
@@ -0,0 +1,21 @@
+public class Unary {
+	public static void main (String [] args){
+		
+		int a = 10;
+		int b = 15;
+		
+		a++;
+		b++;
+		++a;
+		++b;
+		--a;
+		--b;
+		a--;
+		b--;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+	
+}
