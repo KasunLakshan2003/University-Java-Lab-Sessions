c78d441f0173e8616a0782edecf34de13587d497
KasunXploit SL <lakshank228@gmail.com>
2026-01-31 00:54:04 +0530

    Add PrintVariables class to demonstrate variable types

---
+++ Basics/PrintVariables.java
@@ -0,0 +1,29 @@
+public class PrintVariables {
+	
+	public static void main(String [] ags){
+		
+		int num = 10;
+		double pi = 3.14;
+		String name = "Kasun Lakshan";
+		char cht = '%';
+		long longNum = 5902565;
+		short snum = 25963;
+		byte bvar = 9;
+		float fnum = 3.14f;
+		boolean gates = true;
+		
+		System.out.println("Integer type number is :"+num);
+		System.out.println("Double type number is :"+pi);
+		System.out.println("String type Variable is :"+name);
+		System.out.println("Character type variable is :"+num);
+		System.out.println("Long type number is :"+longNum);
+		System.out.println("Short type number is :"+snum);
+		System.out.println("Byte type number is :"+bvar);
+		System.out.println("Float type number is :"+fnum);
+		System.out.println("boolean type variable is :"+gates);
+		
+		
+		
+		
+	}
+}
