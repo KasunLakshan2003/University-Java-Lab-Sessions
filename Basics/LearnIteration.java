c6cf2e2ea2dfc727f20fd44f3d0e33d8433240ee
KasunXploit SL <lakshank228@gmail.com>
2026-02-16 23:27:20 +0530

    Create LearnIteration.java

---
+++ Basics/LearnIteration.java
@@ -0,0 +1,11 @@
+import java.lang.Thread;
+public class LearnIteration{
+	public static void main(String [] args) throws exception{
+		//for loop
+		
+		for (int n = 1 ; n <= 10 ; n++){
+			System.out.println(n);
+			Thread.sleep(1000);
+		}
+	}
+}
