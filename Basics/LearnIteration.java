daf3e4980020213c0d629647ff02959f1f693051
KasunXploit SL <lakshank228@gmail.com>
2026-02-17 11:24:09 +0530

    Update LearnIteration.java

--- Basics/LearnIteration.java
+++ Basics/LearnIteration.java
@@ -1,11 +1,10 @@
-import java.lang.Thread;
-public class LearnIteration{
-	public static void main(String [] args) throws exception{
-		//for loop
-		
-		for (int n = 1 ; n <= 10 ; n++){
-			System.out.println(n);
-			Thread.sleep(1000);
-		}
-	}
+public class LearnIteration {
+    public static void main(String[] args) throws Exception {
+
+        // for loop
+        for (int n = 1; n <= 10; n++) {
+            System.out.println(n);
+            Thread.sleep(1000); // 1 second delay
+        }
+    }
 }
